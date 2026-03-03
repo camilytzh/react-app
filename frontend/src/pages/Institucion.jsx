1c045abf3b5ed4f27ff4c900fc8eabae63c17208
Camily <tctorres@espol.edu.ec>
2026-03-03 13:16:27 -0500

    Values cards created

--- frontend/src/pages/Institucion.jsx
+++ frontend/src/pages/Institucion.jsx
@@ -1,7 +1,10 @@
 const Institucion = () => {
   const aboutUsText = "text-gray-700 text-xs lg:text-sm leading-relaxed mb-2 flex text-justify"; 
+  const valueSubtitle = "text-lg font-bold text-gray-800 mb-2";
+  const valueDescription = "text-gray-600 text-xs lg:text-sm leading-relaxed";
+  const cardStyle = "bg-white p-6 rounded-xl shadow-md text-center border-l-4 border-b-4 border-white hover:border-blue-950 transition-colors duration-300";
   return (
-    <div>
+    <div className="bg-gray-100">
       <div className="relative w-full h-64 lg:h-90">
         <img src="/src/assets/about-us-img.png" alt="About Us" className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black/10"></div>
@@ -13,6 +16,31 @@ const Institucion = () => {
           <p className={aboutUsText}>
             Trabajamos para garantizar una movilidad segura, ordenada, eficiente y sostenible, colocando la protección de la vida y el servicio a la ciudadanía como ejes centrales de nuestra gestión.
           </p>
+        </div>
+      </div>
+      <div className="w-full mt-4 px-6 pb-8">
+        <h2 className="text-center text-blue-950 font-bold text-2xl mb-4">Nuestros Valores</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 ">
+          <div className={cardStyle}>
+            <h3 className={valueSubtitle}>Transparencia</h3>
+            <p className={valueDescription}>Gestión clara, ética y abierta a la ciudadanía.</p>
+          </div>
+          <div className={cardStyle}>
+            <h3 className={valueSubtitle}>Responsabilidad</h3>
+            <p className={valueDescription}>Compromiso con los objetivos institucionales y el servicio público.</p>
+          </div>
+          <div className={cardStyle}>
+            <h3 className={valueSubtitle}>Honestidad</h3>
+            <p className={valueDescription}>Actuación basada en la verdad y la justicia.</p>
+          </div>
+          <div className={cardStyle}>
+            <h3 className={valueSubtitle}>Servicio a la Ciudadanía</h3>
+            <p className={valueDescription}>Soluciones ágiles y accesibles para mejorar la movilidad.</p>
+          </div>
+          <div className={cardStyle}>
+            <h3 className={valueSubtitle}>Seguridad Vial</h3>
+            <p className={valueDescription}>La vida como prioridad en cada decisión.</p>
+          </div>
         </div>
       </div>
     </div>  
