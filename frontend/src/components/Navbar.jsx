1d7def57210ee65ae404d23e0858be94507581a3
Camily <tctorres@espol.edu.ec>
2026-03-12 22:46:50 -0500

    Change sidebar styles and hamburger menu position

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,26 +5,29 @@ import { useState } from 'react';
 
 const Navbar = () => {
     const navLinks = "text-white text-sm font-bold hover:text-blue-300";
+    const menuStyle = "text-gray-700 text-sm font-medium hover:text-white hover:bg-blue-400 rounded-sm px-3 py-2 transition-colors";
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
             <nav className="sticky top-0 bg-blue-950 text-white px-4 py-2.5 z-50 shadow-md shadow-gray-900">
-                <div className="max-w-7xl mx-auto flex items-center justify-between">
+                <div className="max-w-7xl mx-auto flex items-center">
+                    <div className='flex items-center gap-2'>
+                        <button className='md:hidden' onClick={() => setMenuOpen(true)}>
+                            <img src={menu} alt="Menu" className='h-8 w-8 hover:scale-110 transition-transform cursor-pointer'/>    
+                        </button>
+                    </div>
+
                     <Link to="/">
                         <img src={logo} alt="Logo" className="h-12 w-auto"/>
                     </Link>
-                    <ul className=" hidden md:flex gap-10">
+
+                    <ul className=" hidden md:flex gap-10 ml-auto">
                         <li><Link to="/" className={navLinks}>INICIO</Link></li>
                         <li><Link to="/institucion" className={navLinks}>INSTITUCIÓN</Link></li>
                         <li><Link to="/servicios" className={navLinks}>SERVICIOS</Link></li>
                         <li><Link to="/contacto" className={navLinks}>CONTACTO</Link></li>
                     </ul>
-
-                    <button className='md:hidden' onClick={() => setMenuOpen(true)}>
-                        <img src={menu} alt="Menu" className='h-8 w-8 hover:scale-110 transition-transform cursor-pointer'/>    
-                    </button>
-
                 </div>
             </nav>
 
@@ -32,19 +35,19 @@ const Navbar = () => {
                 <div className="fixed inset-0 bg-black/50 z-40" onClick={() => setMenuOpen(false)}/>
             )}
 
-            <div className={`fixed top-0 left-0 h-full w-64 bg-blue-950 z-50 transform transition-transform duration-300
+            <div className={`fixed top-0 left-0 h-full w-64 bg-white z-50 transform transition-transform duration-300
                 ${menuOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                <div className="p-6 flex flex-col gap-6">
-                <Link to="/" onClick={() => setMenuOpen(false)} className={navLinks}>
+                <div className="p-6 flex flex-col">
+                <Link to="/" onClick={() => setMenuOpen(false)} className={menuStyle}>
                     INICIO
                 </Link>
-                <Link to="/institucion" onClick={() => setMenuOpen(false)} className={navLinks}>
+                <Link to="/institucion" onClick={() => setMenuOpen(false)} className={menuStyle}>
                     INSTITUCIÓN
                 </Link>
-                <Link to="/servicios" onClick={() => setMenuOpen(false)} className={navLinks}>
+                <Link to="/servicios" onClick={() => setMenuOpen(false)} className={menuStyle}>
                     SERVICIOS
                 </Link>
-                <Link to="/contacto" onClick={() => setMenuOpen(false)} className={navLinks}>
+                <Link to="/contacto" onClick={() => setMenuOpen(false)} className={menuStyle}>
                     CONTACTO
                 </Link>
                 </div>
