7e798ad546e2cb2a1c8fd92e135cccc2aeef58b7
Camily <tctorres@espol.edu.ec>
2026-03-17 13:31:42 -0500

    Initial server and dependencies added for backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello, API is running!'});
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
