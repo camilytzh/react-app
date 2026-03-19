70111c007e3f8e739f3817a2179a06cb416fef39
Camily <tctorres@espol.edu.ec>
2026-03-18 23:31:50 -0500

    Insert new user in db

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,14 +6,22 @@ const register = async (req, res) => {
     const { nombre, apellido, email, password, cedula } = req.body;
 
     try {
-        const [existingUser] = await db.query('SELECT id FROM users WHERE email = ? OR cedula = ?', [email, cedula]);
+        const [existingUser] = await db.query(
+            'SELECT id FROM users WHERE email = ? OR cedula = ?', 
+            [email, cedula]);
 
         if (existingUser.length > 0) {
             return res.status(400).json({ message: 'El email o cédula ya están en uso' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        
+
+        const [result] = await db.query(
+            'INSERT INTO users (nombre, apellido, email, password, cedula) VALUES (?, ?, ?, ?, ?)',
+            [nombre, apellido, email, hashedPassword, cedula],
+        );
+        res.status(201).json({ message: 'Usuario registrado exitosamente', id: result.insertId });
+
     } catch (err) {
         res.status(500).json({ message: 'Error en el servidor', err });
     }
