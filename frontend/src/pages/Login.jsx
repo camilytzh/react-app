6488ecc1bffc9d08494696099bd6104470e77d54
Camily <tctorres@espol.edu.ec>
2026-04-07 22:36:11 -0500

    login image

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,15 @@
+import loginImage from '../assets/login-img.png';
+
 const Login = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="flex h-screen overflow-hidden">
+            <div className="w-full lg:w-3/5 bg-white">
+                <h2 className="text-2xl font-bold text-gray-800 mb-6">Iniciar Sesión</h2>
+            </div>
+            <div className="hidden lg:block lg:w-2/5">
+                <img src={loginImage} alt="Login section" className="w-full h-full object-cover" />
+            </div>
+            
         </div>
     );
 }
